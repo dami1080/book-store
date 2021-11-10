b972c4e1130b42d8c8cd86ab68ead9aee2a99c6b
dami1080 <dareakintoye@gmail.com>
2021-11-10 10:08:06 -1100

    Add Remove button to the books

--- src/components/Books/Books.js
+++ src/components/Books/Books.js
@@ -1,18 +1,29 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import BookForm from '../NewBooks/BookForm';
+import BookItem from './BookItem';
 
-const Books = () => {
+const Books = (props) => {
+  const { items } = props;
   return (
     <div>
       <ul>
-          <li>Category</li>
-          <li>Title</li>
-          <li>Author</li>
+        {items.map((data) => (
+          <BookItem
+            key={data.id}
+            category={data.category}
+            title={data.title}
+            author={data.author}
+          />
+        ))}
       </ul>
-      <button type="button">Remove</button>
       <BookForm />
     </div>
   );
-}
+};
+Books.propTypes = {
 
+  items: PropTypes.arrayOf(PropTypes.object).isRequired,
+
+};
 export default Books;
